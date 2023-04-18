145dd3e5764e6cf8fd867b54c94c360ab988154c
mustaphachqoubi <mustaphachqoubi88@gmail.com>
2023-04-18 18:44:20 +0000

    i just tested something

--- src/components/HeroSection/HeroBanner/HeroBanner.jsx
+++ src/components/HeroSection/HeroBanner/HeroBanner.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Similar from "./Similar";
 import { AiOutlineArrowDown } from "react-icons/ai";
 import { FaPlay } from "react-icons/fa";
